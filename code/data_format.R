1a614e3735a8297908ab2814fca00a844d30e505
bbaiser <benj.baiser@gmail.com>
2020-09-03 14:00:12 -0400

    import salamnder data, working on joining data sets. starting with changing names

--- code/data_format.R
+++ code/data_format.R
@@ -1,8 +1,16 @@
+#Libraries
+
+library(dplyr)
+library(tidyr)
+library(plyr)
+
 # Load data from Read et al. (2018) from Figshare web archive
 dat <- read.csv('https://ndownloader.figshare.com/files/9167548')
+
+
 # Keep only sites "HARV" and "JORN" using the filter function, and select the relevant columns required  # by the function.
 # Use the mutate function to add a new column named "log_weight" to log-transform the measurements.
-library(dplyr)
+
 dat <- dat %>%
   filter(siteID %in% c('HARV','JORN')) %>%
   select(siteID, taxonID, weight) %>%
@@ -14,4 +22,38 @@ dat %>%
   group_by(siteID, taxonID) %>%
   slice(1)
 
+#look at data that is input for OSTATS functions
 head(dat)
+
+
+#salamander data----
+
+#individual svl measurements
+sal_SVL<-read.csv("./data/MasterSVLData.csv")
+
+head(sal_SVL)
+
+#sire data
+sal_site<-read.csv("./data/sitedata.csv")
+
+head(sal_site)
+
+#rename latitude and longitude column names in sal_site to match sal_SVL
+sal_site<-dplyr::rename(sal_site,
+              Latitude = Lat2,
+              Longitude = Long2)
+
+#make a new column for a concatination of lat and long called lat_long 
+sal_site <- sal_site%>% mutate(
+            lat_long = paste(Latitude, Longitude, sep = "_"))
+
+#look for duplicates
+length(unique(sal_site$lat_long))
+sal_site$lat_long[duplicated(sal_site$lat_long)]
+
+#filter out all duplicate rows (i.e., both pairs of the duplicate)
+singletons <- names(which(table(sal_site$lat_long) == 1))
+final_site<-sal_site[sal_site$lat_long %in% singletons, ]
+
+
+
