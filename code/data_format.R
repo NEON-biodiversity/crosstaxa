02581cbde9e545b42f77b6c8ed5ececc58516443
bbaiser <benj.baiser@gmail.com>
2020-09-03 14:00:12 -0400

    import salamnder data, working on joining data sets. starting with changing names

--- code/data_format.R
+++ code/data_format.R
@@ -1,8 +1,16 @@
+#Libraries
+
+library(dplyr)
+library(tidyr)
+library(plyr)
+
 # Load data from Read et al. (2018) from Figshare web archive
 dat <- read.csv('https://ndownloader.figshare.com/files/9167548')
+
+
 # Keep only sites "HARV" and "JORN" using the filter function, and select the relevant columns required  # by the function.
 # Use the mutate function to add a new column named "log_weight" to log-transform the measurements.
-library(dplyr)
+
 dat <- dat %>%
   filter(siteID %in% c('HARV','JORN')) %>%
   select(siteID, taxonID, weight) %>%
@@ -14,4 +22,22 @@ dat %>%
   group_by(siteID, taxonID) %>%
   slice(1)
 
+#look at data that is input for OSTATS functions
 head(dat)
+
+
+#salamander data
+
+sal_SVL<-read.csv("./data/MasterSVLData.csv")
+
+head(sal_SVL)
+
+sal_site<-read.csv("./data/sitedata.csv")
+
+head(sal_site)
+
+rename(sal_site$Latitude2 = Latitude), Longitude2 = Longitude)
+
+dplyr::rename(sal_site,
+  Latitude = Latitude2, # better naming conventions
+  Longitude = Longitude2)
